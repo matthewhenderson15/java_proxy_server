43134c60b6f491a335a04d3dded7c335c6f98a33
Matthew Henderson <info@matthewahenderson.com>
2021-12-14 00:17:57 -0500

    test commit with new pat

---
+++ Proxy.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+ // Intercepts traffic
+ // Update content for client or server
+ // Create statistics about traffic
+ // Monitor traffic
+
+ public class Proxy {
+     public static void main(String[] args) throws IOException {
+         // Main method logic here
+     } 
+ }
